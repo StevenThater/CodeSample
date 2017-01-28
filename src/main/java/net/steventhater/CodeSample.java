cb8ac29c3ff1a07091f7d4c83c3bfef9af03debd
Steven Thater <s.thater@gmail.com>
2017-01-27 16:26:11 -0900

    Add main class

---
+++ src/main/java/net/steventhater/CodeSample.java
@@ -0,0 +1,15 @@
+package net.steventhater;
+
+/**
+ * Created by Steven Thater on 1/27/2017.
+ */
+public final class CodeSample {
+
+    public static void main(String[] args) {
+        new CodeSample();
+    }
+
+    private CodeSample() {
+    }
+
+}
